b43bc228f48ac44a3ba314e419a3b64fdd59cb2f
Stringbean1413 <61108785+Stringbean1413@users.noreply.github.com>
2020-02-16 18:09:13 +0800

    1. first up load python example
    1. first up load python example

---
+++ py_03_circle.py
@@ -0,0 +1,24 @@
+# five circle
+import turtle
+
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(120, 0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(240, 0)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(60, -60)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(180, -60)
+turtle.pendown()
+turtle.circle(50)
